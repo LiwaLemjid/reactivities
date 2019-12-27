9e08f48204326348b1b5d3cfa5aec3b281c4ab52
LEMJID Liwa <liwa.lemjid@esprit.tn>
2019-12-27 16:37:09 +0100

    Initial Commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public WeatherForecastController(DataContext context)
+        {
+            this.context = context;
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> GetValues()
+        {
+            var values = await this.context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> GetValue(int id)
+        {
+            var value = await this.context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+/*
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        */
+    }
+}
